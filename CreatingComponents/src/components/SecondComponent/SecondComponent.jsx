693f64c8eed6e2bfb5fa15b31fba0cbb941c4e48
NINGAYYA MATHAPATI <ningayyamathapati2000@gmail.com>
2024-01-22 17:56:42 +0530

    first component

---
+++ CreatingComponents/src/components/SecondComponent/SecondComponent.jsx
@@ -0,0 +1,15 @@
+import '../FirstComponent.css'
+export default function SecondComponent(){
+    return(
+        <>
+            {/* <h3>First Component</h3>
+            You have to import this in app.js  */}
+
+            <div className="firstComponent" >
+                <h3>Component heading</h3>
+                <p>Component Body</p>
+            </div>
+            
+        </>
+    );
+}
